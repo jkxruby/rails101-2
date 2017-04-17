835e8e937f1613cb767646cbb8ec17e0beef9745
jkxruby <jkxruby@gmail.com>
2017-04-17 23:21:11 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ resources :groups do
 member do
   post :join
   post :quit
-end 
+end
 
   resources :posts
 end
+
+namespace :account do
+  resources :groups
+end
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
